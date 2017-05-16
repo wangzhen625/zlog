f939bdf7c44f91e04e1e99052aec428e0ce2d4f8
unknown <>
2017-05-16 15:13:59 +0800

    change some definition name

--- logging.go
+++ logging.go
@@ -1,8 +1,10 @@
 package zlog
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"runtime"
 	"strings"
@@ -10,24 +12,28 @@ import (
 )
 
 const (
-	DEBUG_LOG = iota
-	TRACE_LOG
-	INFO_LOG
-	ERROR_LOG
-	FATAL_LOG
+	DebugLevel = iota
+	TraceLevel
+	InfoLevel
+	ErrorLevel
+	FatalLevel
 )
 
+// return string base of log level
 var severityName = []string{
-	DEBUG_LOG: "Debug",
-	TRACE_LOG: "Trace",
-	INFO_LOG:  " Info",
-	ERROR_LOG: "Error",
-	FATAL_LOG: "Fatal",
+	DebugLevel: "Debug",
+	TraceLevel: "Trace",
+	InfoLevel:  " Info",
+	ErrorLevel: "Error",
+	FatalLevel: "Fatal",
 }
 
 type Logger struct {
 	logLevel int
 	depth    int
+	bufOne   bytes.Buffer
+	bufTwo   bytes.Buffer
+	curbuf   *bytes.Buffer
 }
 
 var logger Logger
@@ -50,7 +56,7 @@ func InitLogger(rootPath string, level int) {
 	logger.depth = defaultCallDepth
 	logFileProperty.rootPath = rootPath
 
-	if level < DEBUG_LOG || level > FATAL_LOG {
+	if level < DebugLevel || level > FatalLevel {
 		panic("Logger is not supported")
 	}
 	logger.logLevel = level
@@ -63,6 +69,14 @@ func InitLogger(rootPath string, level int) {
 	messages = make(chan string)
 }
 
+func SetOutput(out io.Writer) {
+
+}
+
+func timeoutFlush(timeout time.Duration) {
+
+}
+
 // call after InitLogger function
 // generally, you needn't change it
 func SetCallDepth(depth int) {
@@ -72,42 +86,42 @@ func SetCallDepth(depth int) {
 }
 
 func Debug(format string, args ...interface{}) {
-	if DEBUG_LOG < logger.logLevel {
+	if DebugLevel < logger.logLevel {
 		return
 	}
-	logger.logFormat(DEBUG_LOG, fmt.Sprintf(format, args...))
+	logger.logFormat(DebugLevel, fmt.Sprintf(format, args...))
 }
 
 func Info(format string, args ...interface{}) {
-	if INFO_LOG < logger.logLevel {
+	if InfoLevel < logger.logLevel {
 		return
 	}
 
-	logger.logFormat(INFO_LOG, fmt.Sprintf(format, args...))
+	logger.logFormat(InfoLevel, fmt.Sprintf(format, args...))
 }
 
 func Error(format string, args ...interface{}) {
-	if ERROR_LOG < logger.logLevel {
+	if ErrorLevel < logger.logLevel {
 		return
 	}
 
-	logger.logFormat(ERROR_LOG, fmt.Sprintf(format, args...))
+	logger.logFormat(ErrorLevel, fmt.Sprintf(format, args...))
 }
 
 func Trace(format string, args ...interface{}) {
-	if TRACE_LOG < logger.logLevel {
+	if TraceLevel < logger.logLevel {
 		return
 	}
 
-	logger.logFormat(TRACE_LOG, fmt.Sprintf(format, args...))
+	logger.logFormat(TraceLevel, fmt.Sprintf(format, args...))
 }
 
 func Fatal(format string, args ...interface{}) {
-	if FATAL_LOG < logger.logLevel {
+	if FatalLevel < logger.logLevel {
 		return
 	}
 
-	logger.logFormat(FATAL_LOG, fmt.Sprintf(format, args...))
+	logger.logFormat(FatalLevel, fmt.Sprintf(format, args...))
 	os.Exit(-1)
 }
 
